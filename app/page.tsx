80c1b09654f31b7e3f30865ad9ee59cd5e1ebfbe
ConnorHardin <chardin993@g.rwu.edu>
2025-08-28 10:46:12 -0400

    Removed Skills Section from Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from '@/components/Hero'
 import FeaturedProjects from '@/components/FeaturedProjects'
-import SkillsOverview from '@/components/SkillsOverview'
 import ContactCTA from '@/components/ContactCTA'
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Hero />
-      <SkillsOverview />
       <FeaturedProjects />
       <ContactCTA />
     </div>
