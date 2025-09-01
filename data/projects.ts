42049b80f88b52db5b96c66193105ce76fe6970e
ConnorHardin <chardin993@g.rwu.edu>
2025-09-01 10:31:42 -0400

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -34,14 +34,22 @@ export const projects: Project[] = [
     githubUrl: 'https://github.com/ConnorHardin/COMSC-225-FinalProject',
     featured: false
   },
-    {
-    id: '6',
+  {
+    id: '4',
     title: 'TradeBind',
     description: 'A platform for union workers, union admins, and contractors to modernize the construction industry.',
     image: '/images/TradeBind.png',
     techStack: ['React', 'Node.js', 'Javascript', 'SQL', 'NGINX'],
     liveUrl: 'https://www.tradebind.com/',
     featured: true
+  },
+    {
+    id: '5',
+    title: 'Worlds Hardest Game',
+    description: 'A challenging game that tests your skills and patience.',
+    techStack: ['TypeScript', 'Node.js', 'HTML', 'CSS'],
+    githubUrl: 'https://github.com/ConnorHardin/WorldsHardestGame',
+    featured: false
   }
 ]
 
